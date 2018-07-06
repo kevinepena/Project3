3f64d3351ca7fff82fa60362b2c02c5975f9cd36
erikamats <erikamats@hotmail.com>
2018-07-05 19:51:53 -0500

    updated PageHeader to Header

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
-import Header from "./components/PageHeader";
+import Header from "./components/Header";
 import Nav from "./components/Nav";
 import Home from "./pages/Home";
 
